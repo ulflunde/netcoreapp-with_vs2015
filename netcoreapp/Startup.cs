662412de69172a57dca07dfc54f12a4be4b51271
Ulf Lunde <E214595@PC35958>
2017-03-21 09:39:47 +0100

    Add project files.

---
+++ netcoreapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(netcoreapp.Startup))]
+namespace netcoreapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
